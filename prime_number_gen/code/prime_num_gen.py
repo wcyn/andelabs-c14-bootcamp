020b3d50f3c35e0b907fc0ce0c2dacfc8634a154
Cynthia Wasonga <wasonga.cynthia@gmail.com>
2017-01-17 09:13:16 +0000

    Added more tests. 3 failing, as they should

--- prime_number_gen/code/prime_num_gen.py
+++ prime_number_gen/code/prime_num_gen.py
@@ -1,21 +1,20 @@
 def prime_num_gen(n):
     """
     This function takes a given number n as an argument
-    and generates prime numbers ranging from zero
-    to n
+    and generates prime numbers ranging from zero to n
     Prime Numbers: A number with only two factors - one and itself
     """
-    if n < 0:
-        return None
     
+    prime_nums = []
     try:
         for i in range(n):
             pass
-        
     except TypeError as e:
         # n is not an integer
         raise(e)
-    return []
+    return prime_nums
+    
+# print(prime_num_gen(20))
         
         
         
--- prime_number_gen/tests/test_prime_num_gen.py
+++ prime_number_gen/tests/test_prime_num_gen.py
@@ -4,7 +4,7 @@ from code.prime_num_gen import prime_num_gen
 
 class TestPrimeNumberGen(unittest.TestCase):
 	def test_returns_list(self):  
-		""" test that the function returns a list"""
+		""" test that the function returns a list if input is positive integer"""
 		# self.assertEqual('bar', Foo().bar())
 		self.assertIsInstance(prime_num_gen(3),list)
 	
@@ -14,6 +14,21 @@ class TestPrimeNumberGen(unittest.TestCase):
 			
 	def test_negative_input_return_none(self):
 		self.assertEqual(None, prime_num_gen(-3))
+		
+	def test_returns_correct_output_1(self):
+		self.assertEqual([2,3,5,7], prime_num_gen(7))
+		
+	def test_returns_correct_output_for_2(self):
+		# 1 is not a prime number, but two is even if it's even
+		self.assertEqual([2], prime_num_gen(2))
+		
+	def test_returns_correct_output_for_0(self):
+		# should return an empty list
+		self.assertEqual([], prime_num_gen(0))
+	
+	def test_returns_correct_output_for_1(self):
+		# should return an empty list
+		self.assertEqual([], prime_num_gen(1))
 		
 if __name__ == '__main__':
 	unittest.main()
