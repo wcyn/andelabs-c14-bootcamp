f788df2830653710c07b7964d89fe061448a4a00
Cynthia Wasonga <wasonga.cynthia@gmail.com>
2017-01-17 09:15:12 +0000

    Added functionality to empty function prime_num_gen

--- prime_number_gen/code/prime_num_gen.py
+++ prime_number_gen/code/prime_num_gen.py
@@ -5,10 +5,21 @@ def prime_num_gen(n):
     Prime Numbers: A number with only two factors - one and itself
     """
     
-    prime_nums = []
+        
     try:
-        for i in range(n):
-            pass
+        prime_nums = [2] # initialize with 2 since n is greater than 2 thus far
+        for i in range(3,n+1):
+            # start from 3 onwards
+            # "outer loop"
+            factor_count=0
+            for x in range (2,i):
+                # start from 2 onwards
+                if i % x == 0:
+                    factor_count+=1
+                    # inner loop breaking..
+                    break # i is not a prime number 
+            if factor_count==0:
+                prime_nums.append(i)
     except TypeError as e:
         # n is not an integer
         raise(e)
